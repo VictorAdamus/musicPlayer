6f99a968f1cbdc4ea2011d7061927bdb0b561424
Victor Adamus <ak47unit@gmail.com>
2023-08-08 21:39:48 +0300

    police mode

--- src/app/page.js
+++ src/app/page.js
@@ -278,7 +278,7 @@ export default function Home() {
     <div className={`w-screen min-h-full h-full duration-500 p-10 relative ${ darkTheme ? 'bg-black' : 'bg-gray-200' }`}>
       <div className='flex flex-row justify-center gap-16 rte:flex-col rte:gap-1 rte:justify-start rte:items-center'>
       <div className={`container relative max-w-md max-h-min p-4 py-10 flex  rounded flex-col justify-start  items-center shadow-xl ${ darkTheme ? 'bg-gray-600' : 'bg-gray-300' } ${isPlaying && policeMode && 'animate-colorPulseRed'}`}>
-        <div className={`absolute left-2 top-2 flex h-4 cursor-pointer w-16 border border-black  rounded-md ${isPlaying && 'animate-bounce'} ${policeMode && 'shadow-police'}`} onClick={()=>{setPoliceMode(!policeMode)}}>
+        <div className={`absolute left-2 top-5 flex h-4 cursor-pointer w-16 border border-black  rounded-md ${isPlaying && 'animate-bounce'} ${policeMode && 'shadow-police'}`} onClick={()=>{setPoliceMode(!policeMode)}}>
           <div className='w-1/2 border-r border-black rounded-s-md h-full bg-blue-600'></div>
           <div className='w-1/2 border-l border-black rounded-e-md h-full bg-red-600'></div>
         </div>
